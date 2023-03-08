7f40cc955a7009e732303c4d11336ad42229fcce
Beatless <54404458+vjpthoda@users.noreply.github.com>
2023-03-08 15:40:52 +0700

    product

---
+++ WebNTQ/Areas/Admin/Controllers/ProductUserController.cs
@@ -0,0 +1,27 @@
+﻿using Model.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace WebNTQ.Areas.Admin.Controllers
+{
+    public class ProductUserController : Controller
+    {
+        // GET: Admin/ProductUser
+        public ActionResult Index(string searchString, bool roleFiler = false, int page = 1, int pageSize = 10)
+        {
+            var dao = new ProductDao();
+            var model = dao.ListAllPaging(searchString, roleFiler, page, pageSize);
+            if (roleFiler)
+            {
+                model = model.Where(x => x.Trending == true).ToPagedList(page, pageSize);
+            }
+            ViewBag.SearchString = searchString;
+            ViewBag.Trending = roleFiler;
+            return View(model);
+        }
+    }
+}
--- WebNTQ/Controllers/HomeController.cs
+++ WebNTQ/Controllers/HomeController.cs
@@ -1,30 +1,27 @@
-﻿using System;
+﻿using Model.Dao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace WebNTQ.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string searchString, bool roleFiler = false, int page = 1, int pageSize = 8)
         {
-            return View();
+            var dao = new ProductDao();
+            var model = dao.ListAllPaging(searchString, roleFiler, page, pageSize);
+            if (roleFiler)
+            {
+                model = model.Where(x => x.Trending == true).ToPagedList(page, pageSize);
+            }
+            ViewBag.SearchString = searchString;
+            ViewBag.Trending = roleFiler;
+            return View(model);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
